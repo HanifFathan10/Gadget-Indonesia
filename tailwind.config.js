ae3675ac4bed4734c12954c336e8f97a237065be
hanif fathan <haniffathan33@gmail.com>
2024-01-13 19:34:57 +0700

    refactor/apply design figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
     extend: {},
     colors: {
       color: {
-        primary: "#090b2c",
+        primary: "#34487F",
+        red: "#95000C",
         secondary: "#db4021",
-        dark: "#212121",
+        dark: "#000000",
         stone: "#171717",
+        grey: "#206F6F",
       },
       light: {
-        100: "#ffffff",
+        100: "#FFFDEE",
         200: "#f2f2f2",
         300: "#E0E0E0",
         400: "#D9D9D9",
