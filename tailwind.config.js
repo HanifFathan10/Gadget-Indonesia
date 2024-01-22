bcdc4a1c67285eaa0697ecc64f0939d99a302d4a
hanif fathan <haniffathan33@gmail.com>
2024-01-22 20:27:24 +0700

    refactor/start MVP 1 setup dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,9 @@ module.exports = {
     extend: {},
     colors: {
       color: {
-        primary: "#34487F",
-        red: "#95000C",
-        secondary: "#db4021",
-        dark: "#000000",
-        stone: "#171717",
-        grey: "#206F6F",
-      },
-      light: {
-        100: "#FFFDEE",
-        200: "#f2f2f2",
-        300: "#E0E0E0",
-        400: "#D9D9D9",
-        500: "#828282",
+        darkCharcoal: "#293333",
+        techaqua: "#01C5C7",
+        light: "#ffffff",
       },
     },
   },
